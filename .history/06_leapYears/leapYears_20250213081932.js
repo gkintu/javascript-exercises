17fa08126fec6d8bf72ad0b41572a1e8cd1171c3
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-02-13 08:19:55 +0100

    Add tests for leap year function covering various scenarios

---
+++ .history/06_leapYears/leapYears_20250213081932.js
@@ -0,0 +1,13 @@
+const leapYears = function(num) {
+   if (num % 4 === 0 && (num % 400 === 0 || num % 100 !== 0)) {
+      return true
+       }
+       return false
+};
+
+// Do not edit below this line
+module.exports = leapYears;
+
+// looks like i need 3 conditions
+// divisble by 4 OR 400 but !NOT 100
+// return 
